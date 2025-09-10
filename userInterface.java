7b36fc064fdf16f6b8103e28f78deb5e23d1c158
cameaye <cayers0704@gmail.com>
2025-09-10 10:14:49 -0400

    Fill skeletons of search methods

--- userInterface.java
+++ userInterface.java
@@ -18,6 +18,8 @@ public class userInterface{
             System.out.println("Please enter the number with your desired functionality: ");
             System.println("1) Print all books in stock \n
                             2) Search Books by ISBN13 \n
+                            3) Search Books by Author \n
+                            4) Search Books by Title \n
                             -1) Exit");
             userInterfaceSelector = lineInput.nexInt();
 
@@ -30,6 +32,16 @@ public class userInterface{
                     System.out.print("Please enter the ISBN13 you would like to search for: ")
                     String isbn = lineInput.next();
                     searchByISBN(books, isbn);
+                    break;
+                case 3:
+                    System.out.print("Please enter the Author you would like to search for: ")
+                    String author = lineInput.next();
+                    searchByAuthor(books, author);
+                    break;
+                case 4:
+                    System.out.print("Please enter the Title you would like to search for: ")
+                    String title = lineInput.next();
+                    searchByTitle(books, title);
                     break;
                 case -1:
                     System.out.println("Thank you for using our service!");
@@ -58,11 +70,19 @@ public class userInterface{
     }
 
     public static void searchByAuthor(ArrayList<bookType> books, String author){
-
+        for(int i = 0; i < books.size(); i++){
+            if( (books.get(i).getAuthor()).equals(author)){
+                books.get(i).printBookDetails();
+            }
+        }
     }
 
     public static void searchByTitle(ArrayList<bookType> books, String title){
-
+        for(int i = 0; i < books.size(); i++){
+            if( (books.get(i).getTitle()).equals(title)){
+                books.get(i).printBookDetails();
+            }
+        }
     }
 
     public static void fileReader(String fileName, ArrayList<bookType> books){
